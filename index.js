2d292c7b3b0df495526bd2c7fdce59ec381e636f
Chi-AnTai <andy.chian.tai@gmail.com>
2018-04-11 09:37:44 +0800

    finished lecture 45, add store and wrap App by provider

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import { React } from 'react'
 import { AppRegistry } from 'react-native';
+import { Provider} from 'react-redux';
 import App from './App';
-AppRegistry.registerComponent('udemyCourse', () => App);
+import configureStore from './src/store/configureStore'
+
+const store = configureStore();
+
+const RNRedux = () =>(
+  <Provider sotre={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('udemyCourse', () => RNRedux);
--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,4 @@
+import {ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from '../actions/actionTypes';
 
 const initialState = {
   places: [],
@@ -5,7 +6,39 @@ const initialState = {
 };
 const reducer = (state = initialState, action) => {
   switch (action.type){
-      default:
+    case ADD_PLACE:
+      return {
+        ...state,
+        places:state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri: "https://cdn0-techbang.pixfs.net/system/excerpt_images/7191/inpage/123c75d334a959384f782977bfba3c4c.png?1317865368"
+          }
+
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place=>{
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place =>{
+          return place.key === action.key;
+        })
+      };
+    case DESELECT_PLACE:
+      retun {
+        ...state,
+        selectedPlace: null
+      };
+    default:
           return state;
   }
 
